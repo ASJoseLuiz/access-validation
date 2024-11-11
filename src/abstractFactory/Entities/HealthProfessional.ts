b49adab50e89b5d3fcae2c3e951de15d174838b5
fdaiane <franciscasampaio284@gmail.com>
2024-11-11 13:09:10 -0300

    criacao do abstractFactory

---
+++ src/abstractFactory/Entities/HealthProfessional.ts
@@ -0,0 +1,15 @@
+import { Access_level } from "@prisma/client";
+import { Func } from "./Func";
+
+export class HealthProfessional extends Func {
+  constructor(
+    id: string,
+    name: string,
+    email: string,
+    password: string,
+    role_id: string
+  ) {
+    super(id, name, email, password, role_id);
+    this.access_level = Access_level.L4;
+  }
+}
---
+++ src/abstractFactory/Entities/User.ts
@@ -0,0 +1,33 @@
+
+import { Access_level } from "@prisma/client";
+
+export class User {
+  protected name: string;
+  protected id: string;
+  protected email: string;
+  protected password: string;
+  protected role_id: string;
+  protected access_level: Access_level;
+
+  constructor(
+    id: string,
+    name: string,
+    email: string,
+    password: string,
+    role_id: string
+  ) {
+    this.id = id;
+    this.name = name;
+    this.email = email;
+    this.password = password;
+    this.role_id = role_id;
+  }
+
+  public getName(): string {
+    return this.name;
+  }
+
+  public getEmail(): string {
+    return this.email;
+  }
+}
