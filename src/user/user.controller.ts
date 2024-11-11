9ff4243356e416d6d802d6292b1d0ee74a261333
Weverml <mamedeweverton@gmail.com>
2024-11-11 19:56:14 -0300

    creacao do controller-login-user

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,22 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post, UsePipes } from "@nestjs/common";
+import { ZodValidation } from "src/pipes/zod-validation.pipe";
+import {
+  CreateUserBodySchema,
+  createUserBodySchema,
+} from "src/types/user-zod.type";
+import { User } from "src/abstractFactory/Entities/User";
+import { UserFacade } from "src/facade/UserFacade.facade";
 
-@Controller('user')
-export class UserController {}
+@Controller()
+export class UserController {
+  constructor(private readonly userFacade: UserFacade) {}
+
+  @Post()
+  @UsePipes(new ZodValidation(createUserBodySchema))
+  public async handleCreateUser(
+    @Body() body: CreateUserBodySchema
+  ): Promise<User> {
+    const { name, email, password, userType } = body;
+    return this.userFacade.createUser(name, email, password, userType);
+  }
+}
