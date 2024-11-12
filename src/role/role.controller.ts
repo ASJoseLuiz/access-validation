20e49fae9c2deca84ebb6def702334944d6f56b2
José Luiz <joseluix4824@gmail.com>
2024-11-11 22:16:20 -0300

    creation of area and role controllers and appModule mod

--- src/role/role.controller.ts
+++ src/role/role.controller.ts
@@ -1,4 +1,67 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Post,
+  Res,
+  UseGuards,
+  UsePipes,
+} from "@nestjs/common";
+import { Access_level, Role } from "@prisma/client";
+import { Response } from "express";
+import { RoleVerification } from "src/decorators/role.decorator";
+import { RoleFacade } from "src/facade/RoleFacade.facade";
+import { AuthGuard } from "src/guards/authentication.guard";
+import { RolesGuard } from "src/guards/roles.guard";
+import { ZodValidation } from "src/pipes/zod-validation.pipe";
+import {
+  CreateRoleBodySchema,
+  createRoleBodySchema,
+  DeleteRoleBodySchema,
+  deleteRoleBodySchema,
+} from "src/types/role-zod.type";
 
-@Controller('role')
-export class RoleController {}
+@Controller()
+@UseGuards(AuthGuard, RolesGuard)
+export class RoleController {
+  constructor(private readonly roleFacade: RoleFacade) {}
+
+  @Get()
+  @RoleVerification(Access_level.L5)
+  public async handleGetRoles(): Promise<Role[]> {
+    return this.roleFacade.getRoles();
+  }
+
+  @Post()
+  @RoleVerification(Access_level.L5)
+  @UsePipes(new ZodValidation(createRoleBodySchema))
+  public async handleCreateRole(
+    @Body() body: CreateRoleBodySchema,
+    @Res() response: Response
+  ): Promise<void> {
+    const { name, description, access_level } = body;
+    const role = this.roleFacade.createRole(name, description, access_level);
+
+    response.json({
+      message: "Papel criado",
+      role: role,
+    });
+  }
+
+  @Delete()
+  @RoleVerification(Access_level.L5)
+  @UsePipes(new ZodValidation(deleteRoleBodySchema))
+  public async handleDeleteRole(
+    @Body() body: DeleteRoleBodySchema,
+    @Res() response: Response
+  ): Promise<void> {
+    const { access_level } = body;
+    const role = await this.roleFacade.deleteRole(access_level);
+
+    response.json({
+      message: "Papel deletado",
+      role: role,
+    });
+  }
+}
--- src/role/role.module.ts
+++ src/role/role.module.ts
@@ -1,9 +1,29 @@
-import { Module } from '@nestjs/common';
-import { RoleService } from './role.service';
-import { RoleController } from './role.controller';
+import { Module } from "@nestjs/common";
+import { RoleService } from "./role.service";
+import { PrismaService } from "src/prisma/prisma.service";
+import { RoleController } from "./role.controller";
+import { RoleFacade } from "src/facade/RoleFacade.facade";
+import { JwtService } from "@nestjs/jwt";
+import { ConfigService } from "@nestjs/config";
+import { AccessLevelStrategy } from "src/strategy/AccessLevelStrategy.strategy";
+import { UserService } from "src/user/user.service";
+import { AccessLogService } from "src/access-log/access-log.service";
+import { AreaService } from "src/area/area.service";
+import { AccessHierarchyStrategy } from "src/strategy/AccessHierarchyStrategy.strategy";
 
 @Module({
-  providers: [RoleService],
-  controllers: [RoleController]
+  providers: [
+    RoleService,
+    PrismaService,
+    RoleFacade,
+    JwtService,
+    ConfigService,
+    AccessLevelStrategy,
+    AccessHierarchyStrategy,
+    UserService,
+    AccessLogService,
+    AreaService,
+  ],
+  controllers: [RoleController],
 })
 export class RoleModule {}
